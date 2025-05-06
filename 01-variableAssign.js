b33115b33e1addd7d87856fd582cf328b357883d
Archana1222 <110172180+Archana1222@users.noreply.github.com>
2025-05-07 01:34:37 +0530

    01-Variable Assignment
    01-Variable Assignment

---
+++ 01-variableAssign.js
@@ -0,0 +1,30 @@
+console.log("======== Variable ==========");
+
+var myFullName; // Variable Declaration
+console.log("Before Initialization:", myFullName);
+myFullName = "Archana Deokate"; // Variable Initialization
+console.log("After Initialization: ", myFullName);
+
+console.log("========Update Variable=============");
+myFullName = "Archana Kiran Deokate"; // Variable Update
+console.log("Updated my Full Name: ", myFullName);
+
+console.log("=======Declare and Initialize a Variable==============");
+var myLove= "I love only JS";
+console.log(myLove);
+
+console.log("=======Form Controls============");
+var firstName="Archana";
+console.log("First Name: ",firstName);
+var lastName="Deokate";
+console.log("Last Name=", lastName);
+var age="33";
+console.log("Age=", age);
+var address="Pune";
+console.log("Address=", address);
+var pinCode="411041";
+console.log("Pincode=", pinCode);
+var gender="Female";
+console.log("Gender=", gender);
+var isMarried="true";
+console.log("Is Married=", isMarried);
